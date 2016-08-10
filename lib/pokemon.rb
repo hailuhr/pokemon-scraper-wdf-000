af6a93fd81df87bed6764eeff2fcef6b0cb64c0e
Flatiron School <github email address>
2016-08-10 16:53:27 -0400

    first commit

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
+require 'pry'
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    # binding.pry
+  end
+
+  #
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon WHERE pokemon.id = id")
+  end
+
 end
